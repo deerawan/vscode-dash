4b7adfe53c0f830e6131878855add35b9f081694
Budi Irawan <deerawan@gmail.com>
2015-12-20 01:41:36 +0700

    add first try config for javascript, create class for dash

--- src/extension.ts
+++ src/extension.ts
@@ -17,18 +17,24 @@ export function activate(context: vscode.ExtensionContext) {
 	var disposable = vscode.commands.registerCommand('extension.dash', () => {
 		// The code you place here will be executed every time your command is executed
 
-        var editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
+        var dash = new Dash();
+        dash.lookUp();
 
-        var selection = editor.selection;
-        var text = editor.document.getText(selection);
-        var doc = editor.document;
+        // var editor = vscode.window.activeTextEditor;
+        // if (!editor) {
+        //     return;
+        // }
 
-        console.log('Selected text: ' + text);
+        // var selection = editor.selection;
+        // var text = editor.document.getText(selection);
+        // var doc = editor.document;
+        // var config = vscode.workspace.getConfiguration('dash');
 
-        exec('open -g dash-plugin://keys=JavaScript&query=' + text);
+        // console.log(config['javascript']);
+        // console.log('Selected text: ' + text);
+        // console.log('Selected language: ' + doc.languageId);
+
+        // exec("open -g 'dash-plugin://keys=JavaScript&query=" + text + "'");
 	});
 
 	context.subscriptions.push(disposable);
@@ -36,4 +42,36 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+export class Dash {
+
+    public lookUp() {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        var selection = editor.selection;
+        var query = editor.document.getText(selection);
+        var languageId = editor.document.languageId;
+
+        exec('open -g "' + this._getUri(query, languageId) + '"');
+    }
+
+    public _getKeys(languageId: string): string {
+        let config = vscode.workspace.getConfiguration('dash');
+        return config[languageId].join(',');
+    }
+
+    public _getUri(query, languageId) {
+        var uri = 'dash-plugin://query=' + encodeURIComponent(query);
+
+        if (languageId) {
+            let keys = this._getKeys(languageId);
+            uri += '&keys=' + keys;
+        }
+        
+        return uri;
+    }
 }
