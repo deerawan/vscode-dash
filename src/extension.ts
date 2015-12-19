a13c59ec7f6af3371b56c234fe45651002f3b760
Budi Irawan <deerawan@gmail.com>
2015-12-20 01:55:45 +0700

    add coffee, go, clojure docset config

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 import {exec} from 'child_process';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-dash" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.dash', () => {
-		// The code you place here will be executed every time your command is executed
-
+	context.subscriptions.push(vscode.commands.registerCommand('extension.dash', () => {
         var dash = new Dash();
         dash.lookUp();
-
-        // var editor = vscode.window.activeTextEditor;
-        // if (!editor) {
-        //     return;
-        // }
-
-        // var selection = editor.selection;
-        // var text = editor.document.getText(selection);
-        // var doc = editor.document;
-        // var config = vscode.workspace.getConfiguration('dash');
-
-        // console.log(config['javascript']);
-        // console.log('Selected text: ' + text);
-        // console.log('Selected language: ' + doc.languageId);
-
-        // exec("open -g 'dash-plugin://keys=JavaScript&query=" + text + "'");
-	});
-
-	context.subscriptions.push(disposable);
+	}));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
 
@@ -71,7 +40,7 @@ export class Dash {
             let keys = this._getKeys(languageId);
             uri += '&keys=' + keys;
         }
-        
+
         return uri;
     }
 }
