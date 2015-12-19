984d0c6ef2e8ae4802dbf51add6a6c4e48a577ef
Budi Irawan <deerawan@gmail.com>
2015-12-19 23:59:34 +0700

    add basic search by query for selected text

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {exec} from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+	var disposable = vscode.commands.registerCommand('extension.dash', () => {
 		// The code you place here will be executed every time your command is executed
 
         var editor = vscode.window.activeTextEditor;
@@ -25,8 +26,9 @@ export function activate(context: vscode.ExtensionContext) {
         var text = editor.document.getText(selection);
         var doc = editor.document;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Selected text: ' + text + ' in ' + doc);
+        console.log('Selected text: ' + text);
+
+        exec('open -g dash-plugin://keys=JavaScript&query=' + text);
 	});
 
 	context.subscriptions.push(disposable);
